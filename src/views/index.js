9c72cce2442da6166908835e874543a600189699
Lane Olson <laneolson@gmail.com>
2019-02-15 17:25:49 -0800

    fix linting

--- src/views/index.js
+++ src/views/index.js
@@ -3,7 +3,6 @@ import { Route, Link } from 'react-router-dom'
 import Header from '../components/base/Header';
 import MapView from './map'
 import withRoot from '../withRoot';
-import Paper from '@material-ui/core/Paper';
 
 const App = () => (
   <div className="app__container">
