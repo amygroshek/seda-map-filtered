4a94bbc089321a1e332a26e88dedc8e4fc6849d7
Lane <laneolson@gmail.com>
2019-02-19 20:48:42 -0800

    add search box with autocomplete functionality

--- src/components/map-view/MapSearch.js
+++ src/components/map-view/MapSearch.js
@@ -1,9 +1,68 @@
 import React from 'react'
+import { InstantSearch, SearchBox, Hits, Highlight, Configure, connectStateResults, connectSearchBox } from 'react-instantsearch-dom';
+import MenuItem from '@material-ui/core/MenuItem';
+import { connect } from 'react-redux';
+import { onViewportChange } from '../../actions/mapActions';
 
-export default function MapSearch() {
+
+const SearchMenuItem = 
+  ({hit, onClick}) => {
+    return (
+      <MenuItem component='div' onClick={() => onClick(hit)}>
+        <span className="hit-name">
+          <Highlight attribute="name" hit={hit} />
+        </span>
+      </MenuItem>
+    );
+  }
+
+const Results = connectSearchBox(
+  ({ currentRefinement, refine, onClick }) =>
+    currentRefinement ? (
+      <Hits hitComponent={ (hit) => <SearchMenuItem hit={hit.hit} onClick={e => { refine(''); onClick(e)}} /> } />
+    ) : //<div>No query</div>
+    null
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  updateMapViewport: (hit) =>
+    hit ?
+      dispatch(onViewportChange({ 
+        latitude: parseFloat(hit.lat), 
+        longitude: parseFloat(hit.lon),
+        zoom: hit.id.length
+      })) : null
+})
+
+const Search = connect(null, mapDispatchToProps)(
+  ({ updateMapViewport }) => {
+    return (
+      <div>
+        <SearchBox />
+        <Results onClick={(e) => updateMapViewport(e)} />
+      </div>
+    );
+  }
+);
+
+const MapSearch = () => {
   return (
-    <div className="map-search-wrapper" style={{display: 'none'}}>
-      Map Search
+    <div className="map-search-wrapper">
+      <InstantSearch
+        appId="VQGKAQUEHP"
+        apiKey="d57cfd62e7ef2abb89335bf26080e3fd"
+        indexName="dev_seda"
+      >
+        <Configure hitsPerPage={5} />
+        <Search />
+      </InstantSearch>
     </div>
   )
 }
+
+
+
+
+export default MapSearch
+
+
